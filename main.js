d9d8a0e7e57c62c373f7b2b9b339a6f023491583
Batuhan Zorney Zengin <batuah.zengin@tein.com.tr>
2025-01-09 09:11:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+setTimeout(function () {
+  var typed = new Typed("#bkrinfotext", {
+    strings: [
+      "Frontend Developer",
+      "Mobile Developer",
+      "Web Developer",
+      "Varan1",
+    ],
+    typeSpeed: 90,
+    backSpeed: 50,
+    backDelay: 400,
+    loop: true,
+  });
+}, 1200);
